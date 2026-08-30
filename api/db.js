05ca6f2cace43a5874fc6a98d5f89443a3961d9e
ader200 <godsgome@gmail.com>
2026-08-30 01:21:35 -0500

    Update - 2026-08-30 01:21

--- api/db.js
+++ api/db.js
@@ -1,14 +1,7 @@
 import { MongoClient } from 'mongodb';
 
-const uri = process.env.MONGODB_URI;
-let client;
-let db;
-
 export async function getDb() {
-    if (!db) {
-        client = new MongoClient(uri);
-        await client.connect();
-        db = client.db('rifas-gratis');
-    }
-    return db;
+    const client = new MongoClient(process.env.MONGODB_URI);
+    await client.connect();
+    return client.db('rifas-gratis');
 }
